0f189442f6ef67ca2c777103b627ef0a2bdb0827
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-29 14:40:10 +0545

    Create function for temperature tempConversion Solve Related Problems

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let Celsius;
+  Celsius = (temp-32)*5/9;
+
+  roundCelsius = Math.round(Celsius*10)/10;
+  return roundCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let Fahrenheit;
+  Fahrenheit = (temp*(9/5))+32;
+
+  roundFahrenheit = Math.round(Fahrenheit*10)/10;
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
