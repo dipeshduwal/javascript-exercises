863af3dca7b61fb1d84752a25c0b2af1c31226a5
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-29 16:27:05 +0545

    Create a function that returns a specific member of the Fibonacci sequence Modify function to suit different test cases provided

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count;
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let sequence = [0, 1];
+
+    for (let i=2; i<=count; i++){
+        sequence[i]= sequence[i-1]+sequence[i-2];
+    }
+
+    return sequence[count];
 
 };
 
